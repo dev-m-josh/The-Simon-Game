80d16f8c893d888ec08f7b0a1b3be0ce88d5012a
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-06-24 20:40:03 +0300

    The Simon Game starting

---
+++ index.js
@@ -0,0 +1,39 @@
+var buttonColors = ["red", "blue", "green", "yellow"]
+
+function nextSequence () {
+
+    var randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    console.log (randomNumber);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
