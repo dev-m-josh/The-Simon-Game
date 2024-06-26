750d7727c31ce74d76a26e0965c416b680107972
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-06-26 13:07:02 +0300

    updating h1 with changes of the level

--- index.js
+++ index.js
@@ -3,55 +3,67 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-var userclickedPattern = [];
+var userClickedPattern = [];
+
+
+var started = false;
+
+
+var level = 0;
+
+// Using jQuery to detect when a keyboard key has been pressed.
+// Call nextSequence when pressed for first time
+
+$(document).keypress(function() {
+  if (!started) {
+
+    // Changing h1 to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
 $(".btn").click(function() {
 
-    var userChosenColour = $(this).attr("id");
-    userclickedPattern.push(userChosenColour);
-  
-    //when a user clicks on a button, the corresponding sound should be played.
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
 
-  });
-
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
 
 function nextSequence() {
 
-  var randomNumber = Math.floor(Math.random() * 3) + 1;
+  // Increase the level by 1 every time nextSequence() is called.
+  level++;
+
+  // updating the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  // Using jQuery to selecting the button with the same id as the randomChosenColour
-
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  //playing sound for the button selected
-    playSound (randomChosenColour);
+  playSound(randomChosenColour);
 }
 
-  //Adding Animation
+//Playing sound
 
-  function animatePress (currentcolor ){
-    $("#" + currentcolor).addClass("pressed");
-  
-    setTimeout (function() {
-      $("#" +  currentcolor).removeClass("pressed");
-    },100);
-  }
-
-  
 function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
 
-    //
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
-  }
+// Creating animation
 
-
-
-
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
 
 
 
